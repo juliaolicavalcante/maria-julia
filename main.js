7c9aad1f482ff258d71ab052a7f4e38d0967fc5a
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-09-27 11:43:42 -0300

    gre

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ function soma(numero1, numero2){
 
 console. log(soma(6,4));
 
-function area(numero3, numero4){
+function retangular(numero3, numero4){
     return numero3*numero4;
+}
+
+function quadrado(numero5){
+    return numero5**2
 }
