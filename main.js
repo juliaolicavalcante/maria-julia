ff3c8ad4be61b3d470eab929ca6c5e08c29020cd
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-09-27 12:25:25 -0300

    gbf

--- main.js
+++ main.js
@@ -18,4 +18,4 @@ let media=function(valor1, valor2, valor3){
     return (valor1+valor2+valor3)/3;
 }
 let notas= media parseInt(prompt('Qual as suas notas?'));
-document.write(media)
+document.write(notas)
