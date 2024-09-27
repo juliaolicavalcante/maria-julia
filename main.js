df463cbae0c53291c1bee7e676838edb3055f669
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-09-27 11:54:53 -0300

    rg

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ function soma(numero1, numero2){
 
 console. log(soma(6,4));
 function areaQuadrado(lado){
-    return lado**2
+    return lado**2;
 }
 function areaRetangulo(numero3, numero4){
     return numero3*numero4;
 }
 function areaTriangulo(ladoBase, ladoAltura){
-    return ladoBase*ladoAltura/2
+    return ladoBase*ladoAltura/2;
+}
+let media=function(valor1, valor2, valor3){
+    return valor1+valor2+valor3/3;
 }
