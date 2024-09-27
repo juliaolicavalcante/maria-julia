cb420e288cfe85b4955ed311ce0fb6532b24a673
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-09-27 12:00:56 -0300

    dgs

--- main.js
+++ main.js
@@ -14,6 +14,6 @@ function areaTriangulo(ladoBase, ladoAltura){
     return ladoBase*ladoAltura/2;
 }
 let media=function(valor1, valor2, valor3){
-    return valor1+valor2+valor3/3;
+    return (valor1+valor2+valor3)/3;
 }
 console.log(media(1,2,3))
