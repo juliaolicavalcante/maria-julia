b798f055a7756f5542d1b128e2b974e4e2603f54
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-10-04 11:07:06 -0300

    yjet

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-function soma(numero1, numero2){
-   return numero1+numero2;
-}
+// function soma(numero1, numero2){
+//    return numero1+numero2;
+// }
 
-console. log(soma(6,4));
-function areaQuadrado(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(6))
-function areaRetangulo(numero3, numero4){
-    return numero3*numero4;
-}
-console.log(areaRetangulo(6,3))
-function areaTriangulo(ladoBase, ladoAltura){
-    return ladoBase*ladoAltura/2;
-}
+// console. log(soma(6,4));
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(6))
+// function areaRetangulo(numero3, numero4){
+//     return numero3*numero4;
+// }
+// console.log(areaRetangulo(6,3))
+// function areaTriangulo(ladoBase, ladoAltura){
+//     return ladoBase*ladoAltura/2;
+// }
 
-let notas=parseInt(prompt('Qual a sua notas do primeiro trimestre?'))
-let notas2=parseInt(prompt('Qual a sua notas do segundo trimestre?'))
-let notas3=parseInt(prompt('Qual a sua notas do treceiro trimestre?'))
-let media=function(valor1, valor2, valor3){
-    return (valor1+valor2+valor3)/3;
-}
-document.write(media(notas, notas2, notas3));
+// let notas=parseInt(prompt('Qual a sua notas do primeiro trimestre?'))
+// let notas2=parseInt(prompt('Qual a sua notas do segundo trimestre?'))
+// let notas3=parseInt(prompt('Qual a sua notas do treceiro trimestre?'))
+// let media=function(valor1, valor2, valor3){
+//     return (valor1+valor2+valor3)/3;
+// }
+// document.write(media(notas, notas2, notas3));
+
