6cb750d30a403456e485681d8e2ad162f3de99bb
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-10-04 11:50:19 -0300

    k

--- main.js
+++ main.js
@@ -26,7 +26,7 @@
 let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
 let i = 0;
-while(i<11){
+while(i<28){
     console.log(i),
     console.log(estados[i])
     i++;
