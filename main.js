e09ac42cb82ca8ce3a27298507d933f173b55b92
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-10-04 11:25:06 -0300

    gvs

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-// function soma(numero1, numero2){
-//    return numero1+numero2;
-// }
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
 
-// console. log(soma(6,4));
-// function areaQuadrado(lado){
-//     return lado**2;
-// }
-// console.log(areaQuadrado(6))
-// function areaRetangulo(numero3, numero4){
-//     return numero3*numero4;
-// }
-// console.log(areaRetangulo(6,3))
-// function areaTriangulo(ladoBase, ladoAltura){
-//     return ladoBase*ladoAltura/2;
-// }
+console.log(soma(6, 4));
+function areaQuadrado(lado) {
+    return lado ** 2;
+}
+console.log(areaQuadrado(6))
+function areaRetangulo(numero3, numero4) {
+    return numero3 * numero4;
+}
+console.log(areaRetangulo(6, 3))
+function areaTriangulo(ladoBase, ladoAltura) {
+    return ladoBase * ladoAltura / 2;
+}
 
-// let notas=parseInt(prompt('Qual a sua notas do primeiro trimestre?'))
-// let notas2=parseInt(prompt('Qual a sua notas do segundo trimestre?'))
-// let notas3=parseInt(prompt('Qual a sua notas do treceiro trimestre?'))
-// let media=function(valor1, valor2, valor3){
-//     return (valor1+valor2+valor3)/3;
-// }
-// document.write(media(notas, notas2, notas3));
+let notas = parseInt(prompt('Qual a sua notas do primeiro trimestre?'))
+let notas2 = parseInt(prompt('Qual a sua notas do segundo trimestre?'))
+let notas3 = parseInt(prompt('Qual a sua notas do treceiro trimestre?'))
+let media = function (valor1, valor2, valor3) {
+    return (valor1 + valor2 + valor3) / 3;
+}
+document.write(media(notas, notas2, notas3));
 
+let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
