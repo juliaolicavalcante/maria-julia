4bb848bee1827732b4a6269b235c83dddbf5ed08
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-10-04 11:42:08 -0300

    dvs

--- main.js
+++ main.js
@@ -24,3 +24,8 @@
 // document.write(media(notas, notas2, notas3));
 
 let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+let i = 0;
+while(1<10){
+    console.log(i),
+}
