9570be84a3e3f5f555efdf7552f949f3cd7f8bb8
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-10-04 12:03:57 -0300

    brgtfrd

--- main.js
+++ main.js
@@ -26,8 +26,13 @@
 let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
 let i = 0;
-while(i<estados.length){
+while (i < estados.length) {
     console.log(i),
-    console.log(estados[i])
+        console.log(estados[i])
     i++;
+}
+
+for (let j = 0; j < estados.length; j++) {
+    console.log(j),
+    console.log(estados[j])
 }
