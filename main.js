1f79c4aadbc0bf2dc23b9b9eb4b8108f41f38b35
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-10-04 11:43:48 -0300

    dsggs

--- main.js
+++ main.js
@@ -26,6 +26,7 @@
 let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
 let i = 0;
-while(1<10){
+while(i<10){
     console.log(i),
+    i=i+1
 }
